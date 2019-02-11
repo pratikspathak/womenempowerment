ca756d298c0a34561888b120d5b0087e7c2d5936
pratikspathak <pratik.pathak@lntinfotech.com>
2019-02-11 14:44:52 +0530

    User reg

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,23 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddNgoComponent } from './add-ngo.component';
-import { NgoService } from './ngo-service';
-import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { AddUserComponent } from './user/add-user.component';
+import { UserService } from './user/user-service';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddNgoComponent
+    AddUserComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
+    BrowserModule,FormsModule,HttpClientModule
   ],
-  providers: [NgoService,HttpClient],
+  providers: [UserService , HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
